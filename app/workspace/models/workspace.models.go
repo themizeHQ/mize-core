8f7a27380fa3b9ca8627e0897d8ff082f8135fc0
emekarr <aceanyanwu02@gmail.com>
2022-06-06 00:19:22 +0100

    built workspace model

---
+++ app/workspace/models/workspace.models.go
@@ -0,0 +1,31 @@
+package workspace
+
+import (
+	"encoding/json"
+
+	"github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Workspace struct {
+	Id   string `bson:"_id" json:"id"`
+	Name string `bson:"name"`
+	Email string `bson:"email"`
+	Censor string `bson:"censor"`
+	
+	CreatedAt   primitive.Timestamp  `bson:"createdAt"`
+	UpdatedAt   primitive.Timestamp  `bson:"updatedAt"`
+}
+
+func (workspace *Workspace) MarshalBinary() ([]byte, error) {
+	return json.Marshal(workspace)
+}
+
+func (workspace *Workspace) Validate() error {
+	return validation.ValidateStruct(workspace,
+		validation.Field(&workspace.Name, validation.Required.Error("Name is a required field")),
+		validation.Field(&workspace.Email, validation.Required.Error("Email is a required field"), is.Email.Error("Field must be a valid email")),
+	)
+}
+
