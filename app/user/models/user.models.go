25844630d486b0dc7a7e6696a4b067506e04b3e3
emekarr <aceanyanwu02@gmail.com>
2022-06-24 23:09:50 +0100

    reduced required fields to create user to 3

--- app/user/models/user.models.go
+++ app/user/models/user.models.go
@@ -31,11 +31,9 @@ func (user *User) MarshalBinary() ([]byte, error) {
 
 func (user *User) Validate() error {
 	return validation.ValidateStruct(user,
-		validation.Field(&user.FirstName, validation.Required.Error("FirstName is a required field")),
-		validation.Field(&user.LastName, validation.Required.Error("LaststName is a required field")),
 		validation.Field(&user.UserName, validation.Required.Error("UserName is a required field")),
 		validation.Field(&user.Email, validation.Required.Error("Email is a required field"), is.Email.Error("Field must be a valid email")),
-		validation.Field(&user.Region, validation.Required.Error("Please pass in your region"), is.CountryCode2.Error("Please pass in a valid country code")),
+		validation.Field(&user.Region, is.CountryCode2.Error("Please pass in a valid country code")),
 		validation.Field(&user.Password, validation.Length(6, 100).Error("Password cannot be less than 6 digits"), validation.Required.Error("Password is a required field")),
 	)
 }
