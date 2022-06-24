03cf5cf738706e150b56cdf0eac8b7d7c9805992
emekarr <aceanyanwu02@gmail.com>
2022-06-24 02:41:04 +0100

    added login of error to console

--- app_errors/handler.errors.go
+++ app_errors/handler.errors.go
@@ -1,6 +1,8 @@
 package app_errors
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"mize.app/server_response"
 )
@@ -11,5 +13,6 @@ type MizeErrors interface {
 
 func ErrorHandler(ctx *gin.Context, err error, code int) {
 	ctx.Abort()
+	fmt.Println(err)
 	server_response.Response(ctx, code, err.Error(), false, nil)
 }
