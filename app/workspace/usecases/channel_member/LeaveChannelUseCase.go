b8c699e17ac32a306693f6d6e5e814fdea546e1b
emekarr <aceanyanwu02@gmail.com>
2022-11-16 22:16:17 +0100

    imroved the leave channel usecase

--- app/workspace/usecases/channel_member/LeaveChannelUseCase.go
+++ app/workspace/usecases/channel_member/LeaveChannelUseCase.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"mize.app/app/workspace/repository"
 	"mize.app/app_errors"
@@ -14,22 +13,20 @@ import (
 
 func LeaveChannelUseCase(ctx *gin.Context, id *string) bool {
 	channelMemberRepo := repository.GetChannelMemberRepo()
-	exists, err := channelMemberRepo.FindOneByFilter(map[string]interface{}{
+	exists, err := channelMemberRepo.CountDocs(map[string]interface{}{
 		"_id":         *id,
 		"userId":      *utils.HexToMongoId(ctx, ctx.GetString("UserId")),
 		"workspaceId": *utils.HexToMongoId(ctx, ctx.GetString("Workspace")),
-	}, options.FindOne().SetProjection(map[string]int{
-		"_id": 1,
-	}))
+	})
 	if err != nil {
 		app_errors.ErrorHandler(ctx, app_errors.RequestError{Err: errors.New("something went wrong"), StatusCode: http.StatusInternalServerError})
 		return false
 	}
-	if exists == nil {
+	if exists == 0 {
 		app_errors.ErrorHandler(ctx, app_errors.RequestError{Err: errors.New("you are not a member of this channel"), StatusCode: http.StatusUnauthorized})
 		return false
 	}
-	deleted, err := channelMemberRepo.DeleteById(exists.Id.Hex())
+	deleted, err := channelMemberRepo.DeleteById(*id)
 	if err != nil || !deleted {
 		app_errors.ErrorHandler(ctx, app_errors.RequestError{Err: errors.New("could not leave channel"), StatusCode: http.StatusInternalServerError})
 		return false
