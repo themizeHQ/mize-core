9c86339eaf0c968f428194711529776b642a836b
emekarr <aceanyanwu02@gmail.com>
2022-12-22 07:45:10 +0100

    formated time for email schedule reminder

--- schedule_manager/scheduler.go
+++ schedule_manager/scheduler.go
@@ -86,67 +86,66 @@ func ScheduleEmail(payload *scheduleModels.Schedule, workspaceId string) {
 		teamMemberRepo := teamsRepo.GetTeamMemberRepo()
 		wg := sync.WaitGroup{}
 
-		if payload.RemindByEmail {
-			if payload.Recipients != nil {
-				for _, recipient := range *payload.Recipients {
-					wg.Add(1)
-					go func(rcp scheduleModels.Recipients) {
-						defer func() {
-							wg.Done()
-						}()
-						if rcp.Type == scheduleModels.UserRecipient {
-							user, err := userRepo.FindById(rcp.RecipientId.Hex())
-							if err != nil {
-								logger.Error(errors.New("schedule error - could not find user"), zap.Error(err))
-								return
-							}
-							if user == nil {
-								return
-							}
-							emails.SendEmail(user.Email, fmt.Sprintf("Alert reminder from %s", payload.From), "alert_reminder", map[string]interface{}{
-								"TIME":     time.Unix(payload.Time, 0).Local(),
-								"DETAILS":  payload.Details,
-								"NAME":     payload.Name,
-								"LOCATION": payload.Location,
-								"FROM":     payload.From,
-								"URL":      payload.Url,
-							})
-						} else if rcp.Type == scheduleModels.TeamRecipient {
-							members, err := teamMemberRepo.FindMany(map[string]interface{}{
-								"workspaceId": workspaceId,
-								"teamId":      rcp.RecipientId,
-							})
-							if err != nil {
-								logger.Error(errors.New("schedule error - could not find team members"), zap.Error(err))
-								return
-							}
-
-							for _, member := range *members {
-								wg.Add(1)
-								go func(mem teamModels.TeamMembers) {
-									defer func() {
-										wg.Done()
-									}()
-									user, err := userRepo.FindById(mem.UserId.String())
-									if err != nil {
-										logger.Error(errors.New("schedule error -could not create notification"), zap.Error(err))
-										return
-									}
-									emails.SendEmail(user.Email, payload.Name, "alert_reminder", map[string]interface{}{
-										"TIME":     time.Unix(payload.Time, 0).Local(),
-										"DETAILS":  payload.Details,
-										"NAME":     payload.Name,
-										"LOCATION": payload.Location,
-										"FROM":     payload.From,
-										"URL":      payload.Url,
-									})
-								}(member)
-							}
+		if payload.Recipients != nil {
+			formatedTime := time.Unix(payload.Time, 0).Local().Format(time.UnixDate)
+			for _, recipient := range *payload.Recipients {
+				wg.Add(1)
+				go func(rcp scheduleModels.Recipients) {
+					defer func() {
+						wg.Done()
+					}()
+					if rcp.Type == scheduleModels.UserRecipient {
+						user, err := userRepo.FindById(rcp.RecipientId.Hex())
+						if err != nil {
+							logger.Error(errors.New("schedule error - could not find user"), zap.Error(err))
+							return
 						}
-					}(recipient)
-				}
-				wg.Wait()
+						if user == nil {
+							return
+						}
+						emails.SendEmail(user.Email, fmt.Sprintf("Alert reminder from %s", payload.From), "alert_reminder", map[string]interface{}{
+							"TIME":     formatedTime,
+							"DETAILS":  payload.Details,
+							"NAME":     payload.Name,
+							"LOCATION": payload.Location,
+							"FROM":     payload.From,
+							"URL":      payload.Url,
+						})
+					} else if rcp.Type == scheduleModels.TeamRecipient {
+						members, err := teamMemberRepo.FindMany(map[string]interface{}{
+							"workspaceId": workspaceId,
+							"teamId":      rcp.RecipientId,
+						})
+						if err != nil {
+							logger.Error(errors.New("schedule error - could not find team members"), zap.Error(err))
+							return
+						}
+
+						for _, member := range *members {
+							wg.Add(1)
+							go func(mem teamModels.TeamMembers) {
+								defer func() {
+									wg.Done()
+								}()
+								user, err := userRepo.FindById(mem.UserId.String())
+								if err != nil {
+									logger.Error(errors.New("schedule error -could not create notification"), zap.Error(err))
+									return
+								}
+								emails.SendEmail(user.Email, payload.Name, "alert_reminder", map[string]interface{}{
+									"TIME":     formatedTime,
+									"DETAILS":  payload.Details,
+									"NAME":     payload.Name,
+									"LOCATION": payload.Location,
+									"FROM":     payload.From,
+									"URL":      payload.Url,
+								})
+							}(member)
+						}
+					}
+				}(recipient)
 			}
+			wg.Wait()
 		}
 		// remind 30 mins before time
 	}, chrono.WithTime(time.Unix(payload.Time-1800, 0)))
