4e14bd6476f7ccdd96fa800463e5980a3be45803
emekarr <aceanyanwu02@gmail.com>
2023-01-18 21:46:18 +0100

    avoid users creating schedules for others

--- app/schedule/usecases/CreateScheduleUseCase.go
+++ app/schedule/usecases/CreateScheduleUseCase.go
@@ -1,13 +1,19 @@
 package usecases
 
 import (
+	"errors"
 	"fmt"
+	"net/http"
 	"sync"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"mize.app/app/schedule/models"
 	"mize.app/app/schedule/repository"
+	workspaceRepo "mize.app/app/workspace/repository"
+	"mize.app/app_errors"
+	workspace_member_constants "mize.app/constants/workspace"
 	"mize.app/schedule_manager"
 	"mize.app/utils"
 )
@@ -17,6 +23,24 @@ func CreateScheduleUseCase(ctx *gin.Context, payload *models.Schedule) {
 	payload.WorkspaceId = utils.HexToMongoId(ctx, ctx.GetString("Workspace"))
 	payload.CreatedBy = *utils.HexToMongoId(ctx, ctx.GetString("UserId"))
 	payload.From = fmt.Sprintf("%s %s", ctx.GetString("Firstname"), ctx.GetString("Lastname"))
+	if len(*payload.Recipients) != 0 {
+		workspaceRepo := workspaceRepo.GetWorkspaceMember()
+		member, err := workspaceRepo.FindOneByFilter(map[string]interface{}{
+			"admin":  true,
+			"userId": *utils.HexToMongoId(ctx, ctx.GetString("UserId")),
+		}, options.FindOne().SetProjection(map[string]interface{}{
+			"adminAccess": 1,
+		}))
+		if err != nil {
+			app_errors.ErrorHandler(ctx, app_errors.RequestError{Err: errors.New("an error occured while validating user"), StatusCode: http.StatusInternalServerError})
+			return
+		}
+		access := member.HasAccess([]workspace_member_constants.AdminAccessType{workspace_member_constants.AdminAccess.SCHEDULE_ACCESS, workspace_member_constants.AdminAccess.FULL_ACCESS})
+		if !access {
+			app_errors.ErrorHandler(ctx, app_errors.RequestError{Err: errors.New("you do not have authorization to create schedules for other members"), StatusCode: http.StatusInternalServerError})
+			return
+		}
+	}
 	scheduleRepository.CreateOne(*payload)
 	now := time.Now().Unix()
 	end := time.Now().Add(time.Hour * 1).Unix()
