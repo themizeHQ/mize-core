f4b2a254b23c43586584ed13a0b88f1bccc156c9
emekarr <aceanyanwu02@gmail.com>
2023-02-03 19:23:18 +0100

    reduced searched channel payload

--- app/workspace/controllers/channel_member.go
+++ app/workspace/controllers/channel_member.go
@@ -79,7 +79,6 @@ func FetchChannels(ctx *gin.Context) {
 	server_response.Response(ctx, http.StatusOK, "channels retrieved", true, channels)
 }
 
-
 func SearchUserChannels(ctx *gin.Context) {
 	term := ctx.Query("term")
 	channelsRepo := repository.GetChannelMemberRepo()
@@ -93,19 +92,16 @@ func SearchUserChannels(ctx *gin.Context) {
 	}
 	skip := (page - 1) * limit
 	channels, err := channelsRepo.FindManyStripped(map[string]interface{}{
-			"channelName":    map[string]interface{}{"$regex": term, "$options": "im"},
-			"workspaceId": *utils.HexToMongoId(ctx, ctx.GetString("Workspace")),
+		"channelName": map[string]interface{}{"$regex": term, "$options": "im"},
+		"workspaceId": *utils.HexToMongoId(ctx, ctx.GetString("Workspace")),
 	}, &options.FindOptions{
 		Limit: &limit,
 		Skip:  &skip,
 	}, options.Find().SetProjection(
 		map[string]interface{}{
-			"lastMessage":     1,
-			"unreadMessages":  1,
-			"channelId":       1,
-			"channelName":     1,
-			"lastMessageSent": 1,
-			"profileImage":    1,
+			"channelId":    1,
+			"channelName":  1,
+			"profileImage": 1,
 		},
 	))
 	if err != nil {
